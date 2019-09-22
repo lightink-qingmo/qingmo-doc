454762f75f868c9d6008282da5c69c469764ccf3
zoharSoul <hunji66@gmail.com>
2019-09-22 23:49:26 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,27 @@
+
+
+module.exports = {
+
+    title: '个人主页',
+    description: '姜帅杰的博客',
+    head: [
+        ['link', { rel: 'icon', href: '/img/logo.ico' }],
+        ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ],
+
+    themeConfig: {
+        // sidebar: [
+        //     '/',
+        //     '/page-a',
+        //     ['/page-b', 'Explicit link text']
+        // ],
+        // sidebarDepth: 2,
+        // sidebar: sidebar
+    },
+
+    plugins: ['autobar']
+
+}
+
+
+
